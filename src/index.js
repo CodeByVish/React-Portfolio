6b460a20b0ffe1c773119f206c01bffc9215b9d0
Vishakha Maheshwari <129320874+CodeByVish@users.noreply.github.com>
2025-09-19 21:00:04 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './app/App';
-import './index.css';
+import { HashRouter } from "react-router-dom";
+import App from "./app/App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <HashRouter basename="/">
     <App />
+  </HashRouter>
 );
