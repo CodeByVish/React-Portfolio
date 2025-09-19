2397bc5884520922348aa0a10a4481807bea79fa
Vishakha Maheshwari <129320874+CodeByVish@users.noreply.github.com>
2025-09-19 21:48:02 +0400

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,32 +1,29 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BrowserRouter as Router,
-  useLocation,
-} from "react-router-dom";
+import { HashRouter as Router, useLocation } from "react-router-dom";
 import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
-import AnimatedCursor  from "../hooks/AnimatedCursor";
+import AnimatedCursor from "../hooks/AnimatedCursor";
 import "./App.css";
 
-function _ScrollToTop(props) {
+function _ScrollToTop({ children }) {
   const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-  return props.children;
+  return children;
 }
 const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div className="cursor__dot">
         <AnimatedCursor
           innerSize={15}
           outerSize={15}
-          color="255, 255 ,255"
+          color="255, 255, 255"
           outerAlpha={0.4}
           innerScale={0.7}
           outerScale={5}
