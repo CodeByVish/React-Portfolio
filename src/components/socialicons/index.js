4c34b9e0500597ac8b6a5a46fd77fa4b59cb4417
Vishakha Maheshwari <129320874+CodeByVish@users.noreply.github.com>
2025-09-19 19:09:02 +0400

    Update index.js

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -1,63 +1,30 @@
 import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import "./style.css";
-import {
-  FaGithub,
-  FaTwitter,
-  FaFacebookF,
-  FaLinkedin,
-  FaYoutube,
-  FaTwitch,
-} from "react-icons/fa";
-import { socialprofils } from "../../content_option";
 
-export const Socialicons = (params) => {
+// Renders only the socials you actually provided in content_option.js
+export default function SocialIcons({ socialprofils = {} }) {
+  const items = [
+    { key: "github", Icon: FaGithub },
+    { key: "linkedin", Icon: FaLinkedinIn },
+  ];
   return (
-    <div className="stick_follow_icon">
-      <ul>
-        {/* {socialprofils.twitter && (
-          <li>
-            <a href={socialprofils.twitter}>
-              <FaTwitter />
-            </a>
-          </li>
-        )} */}
-        {socialprofils.github && (
-          <li>
-            <a href={socialprofils.github}>
-              <FaGithub />
-            </a>
-          </li>
-        )}
-        {/* {socialprofils.facebook && (
-          <li>
-            <a href={socialprofils.facebook}>
-              <FaFacebookF />
-            </a>
-          </li>
-        )} */}
-        {socialprofils.linkedin && (
-          <li>
-            <a href={socialprofils.linkedin}>
-              <FaLinkedin />
-            </a>
-          </li>
-        )}
-        {socialprofils.youtube && (
-          <li>
-            <a href={socialprofils.youtube}>
-              <FaYoutube />
-            </a>
-          </li>
-        )}
-        {socialprofils.twitch && (
-          <li>
-            <a href={socialprofils.twitch}>
-              <FaTwitch />
-            </a>
-          </li>
-        )}
-      </ul>
-      <p>Follow Me</p>
+    <div className="social-icons">
+      {items.map(({ key, Icon }) => {
+        const href = socialprofils[key];
+        if (!href) return null;
+        return (
+          <a
+            key={key}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={key}
+          >
+            <Icon />
+          </a>
+        );
+      })}
     </div>
   );
-};
+}
